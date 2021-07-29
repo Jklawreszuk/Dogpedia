ea2608452a5ef0a66c4b792f71410aca11ccaf62
jklawreszuk <jk.lawreszuk@gmail.com>
2021-07-29 21:50:15 +0200

    Improve UI and use some Telerik components

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,40 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function input(Name, Type, ContentArr) {
+    $(document).ready(function () {
+
+        let obj;
+        switch (Type) {
+            case "TextBox":
+                obj = {
+                    placeholder: ContentArr[0],
+                    label: {
+                        content: ContentArr[1],
+                        floating: true
+                    }
+                };
+                break;
+            case "Slider":
+                obj = {
+                    increaseButtonTitle: "Zwiększ",
+                    decreaseButtonTitle: "Zmniejsz",
+                    min: ContentArr[0],
+                    max: ContentArr[1],
+                    smallStep: 1,
+                    largeStep: 3
+                };
+                break;
+            default:
+                break;
+        }
+
+        $(Name)[`kendo${Type}`](obj);
+    });
+}
+
+input(".name", "TextBox", ["Wpisz rasę psa tutaj", "Nazwa"]);
+input(".corigin", "TextBox", ["Wpisz pochodzenie psa", "Kraj pochodzenia"]);
+input(".weight", "TextBox", ["Wpisz wagę psa", "Waga"]);
+input(".price", "TextBox", ["Ustal wartość psa", "Cena"]);
+input(".lifespan", "Slider", [0, 100]);
